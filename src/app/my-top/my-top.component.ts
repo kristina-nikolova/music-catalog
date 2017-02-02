39aeab328874533fd3fdfb074eed1eed66ee31d2
Krisi Nikolova <kristina.g.nikolova@gmail.com>
2017-02-03 01:02:18 +0200

    return current mood; bug with missing track cover photo is fixed

--- src/app/my-top/my-top.component.ts
+++ src/app/my-top/my-top.component.ts
@@ -12,30 +12,68 @@ import { MyTopService } from './my-top.service';
 export class MyTopComponent implements OnInit {
   topTracksIds: Object[];
   isDataLoading: boolean;
+  currentMood: string;
   topTracks = [];
 
   constructor(private myTopService: MyTopService,
               private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
+    this.getMood();
     this.loadMyTopTracks();
   }
 
   loadMyTopTracks() {
     const _self = this;
+    
     this.topTracksIds = this.localStorageService.keys();
-    this.isDataLoading = true;
+    
+    if (this.topTracksIds.length) {
+      this.isDataLoading = true;
 
-    this.topTracksIds.forEach(function(id) {
-      _self.myTopService.getTrackById(id)
-          .subscribe(
-              (data) => {
-                _self.topTracks.push(data);
-                _self.isDataLoading = false;
-              },
-              (err) => { console.log(err); }
-           );
-    });
+      this.topTracksIds.forEach(function(id) {
+        _self.myTopService.getTrackById(id)
+            .subscribe(
+                (data) => {
+                  _self.topTracks.push(data);
+                  _self.isDataLoading = false;
+                },
+                (err) => { console.log(err); }
+            );
+      });
+    }
+  }
+
+  getMood() {
+    let moods = [];
+
+    for ( let i = 0, len = this.localStorageService.length(); i < len; ++i ) {
+      let value = this.localStorageService.get( this.localStorageService.keys()[i] );
+      moods.push(value['mood']);
+    }
+
+    this.currentMood = this.getHighlyOccuredElement(moods);
+  }
+
+  getHighlyOccuredElement(array) {
+    if(array.length == 0)
+        return null;
+    let modeMap = {};
+    let maxEl = array[0], maxCount = 1;
+    for(let i = 0; i < array.length; i++)
+    {
+        let el = array[i];
+        if(modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;  
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
   }
 
 }
