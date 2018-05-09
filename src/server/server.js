d72854521f84221dea2b03ba5f86f865d7a89d09
Kristina <kristina.nikolova@mentormate.com>
2018-05-09 20:33:11 +0300

    mongodb is added(WIP)

---
+++ src/server/server.js
@@ -0,0 +1,64 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      path = require('path');
+      cors = require('cors'),
+      mongoose = require("mongoose");
+
+const db = mongo.connect("mongodb://localhost:4200/catalog", function(err, response){  
+   if(err){ console.log( err); }  
+   else{ console.log('Connected to ' + db, ' + ', response); }  
+});  
+
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 
+}
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors(corsOptions));
+
+// app.use(errorHandler) 
+// function errorHandler (err, req, res, next) {
+//   res.status(500);
+//   res.render('error', { error: err });
+// }
+
+app.route('/api/moods').get((req, res) => {
+  res.send({
+    moods: [{ plays: 1 }, { mood: 'happy' }]
+  });
+});
+
+app.route('/api/moods').post((req, res) => {
+  res.send(201, req.body);
+});
+
+// app.route('/api/cats/:name').get((req, res) => {
+//   const requestedCatName = req.params['name'];
+//   res.send({ name: requestedCatName });
+// });
+
+// app.route('/api/moods/:trackId').put((req, res) => {
+//   res.send(200, req.body);
+// });
+
+// app.route('/api/cats/:name').delete((req, res) => {
+//   res.sendStatus(204);
+// });
+
+app.listen(port, () => {
+  console.log('Server started!');
+});
